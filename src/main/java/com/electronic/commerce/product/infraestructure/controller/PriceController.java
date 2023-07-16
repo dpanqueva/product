de1beb1576bcb86dac26d0a20990132111d54e75
Diego Panqueva <daerb9011@gmail.com>
2023-07-16 00:03:57 -0500

    swagger documentation

--- src/main/java/com/electronic/commerce/product/infraestructure/controller/PriceController.java
+++ src/main/java/com/electronic/commerce/product/infraestructure/controller/PriceController.java
@@ -2,6 +2,11 @@ package com.electronic.commerce.product.infraestructure.controller;
 
 import com.electronic.commerce.product.application.PriceService;
 import com.electronic.commerce.product.infraestructure.dto.PriceDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,17 @@ public class PriceController {
     @Autowired
     private PriceService priceService;
 
+    @Operation(summary = "Get a product by applicationDate and productId and brandId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the product",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PriceDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "UPS! Bad request, date must have the format yyyy-mm-ddHH:mm:ss. Example: 2020-06-1410:10:00",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "UPS! Unexpected error, contact system administrator",
+                    content = @Content) })
     @GetMapping
     public ResponseEntity<PriceDTO> getPrice(@RequestParam("applicationDate")
                                              @DateTimeFormat(pattern = "yyyy-MM-ddHH:mm:ss")
